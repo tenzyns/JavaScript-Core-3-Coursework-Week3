029297db1f067d449c31a5b98a0c6a41523a3843
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-11-07 22:20:26 +0000

    Update exercise-1.js

--- exercises/exercise-1/exercise-1.js
+++ exercises/exercise-1/exercise-1.js
@@ -3,8 +3,9 @@ const personOne = {
   age: 34,
   favouriteFood: "Spinach",
 };
+//const {name, age, favouriteFood} = personOne;
 
-function introduceYourself(___________________________) {
+function introduceYourself({ name, age, favouriteFood}) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
